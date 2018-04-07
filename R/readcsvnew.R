1ead67fcb1ad4b1d1635e37a37bd6541b2fd57a7
claraquintero <38136557+claraquintero@users.noreply.github.com>
2018-04-07 13:58:48 -0500

    Update readcsvnew.R

--- R/readcsvnew.R
+++ R/readcsvnew.R
@@ -5,21 +5,13 @@
 #' @export
 #' @param file a csv file.
 #' @param ... arguments passed to read.csv
-#readcsvnew <- function(file, ...){
- # if(!grepl(".csv$", file)){
-  #  stop("El archivo a cargar debe estar en el formato csv!")
-  #}
- # read.csv(file, ...);
-#}
-library(httr)
-library("rjson")
-
-readcsvnew <- function()
-  {
- 
-  json_file <- GET("https://www.datos.gov.co/resource/rxv3-h9d8.json")
-  json_data <- fromJSON(file=json_file)
+readcsvnew <- function(file, ...){
+  if(!grepl(".csv$", file)){
+    stop("El archivo a cargar debe estar en el formato csv!")
   }
+  read.csv(file, ...);
+}
+
 
 
 
