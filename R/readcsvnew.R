a065eb95b171b39806de79ca320d54cb27b18a86
claraquintero <38136557+claraquintero@users.noreply.github.com>
2018-04-07 13:21:27 -0500

    Update readcsvnew.R

--- R/readcsvnew.R
+++ R/readcsvnew.R
@@ -5,9 +5,17 @@
 #' @export
 #' @param file a csv file.
 #' @param ... arguments passed to read.csv
-readcsvnew <- function(file, ...){
-  if(!grepl(".csv$", file)){
-    stop("El archivo a cargar debe estar en el formato csv!")
+#readcsvnew <- function(file, ...){
+ # if(!grepl(".csv$", file)){
+  #  stop("El archivo a cargar debe estar en el formato csv!")
+  #}
+ # read.csv(file, ...);
+#}
+readcsvnew <- function()
+  {
+  r<-GET(https://www.datos.gov.co/api/views/79fi-zm8c/rows.csv?accessType=DOWNLOAD)
+  read.csv(r);
   }
-  read.csv(file, ...);
-}
+
+
+
